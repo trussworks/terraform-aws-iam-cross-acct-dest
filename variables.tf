55032ed6190c9144f11f0ccfdc16842f367bb3ca
kilbergr <kilbergr@users.noreply.github.com>
2019-10-29 17:07:08 -0700

    add create_circleci_role

--- variables.tf
+++ variables.tf
@@ -12,3 +12,9 @@ variable "source_account_role_names" {
   description = "The name of the role that the assume role call will be coming from. Again, this should correspond to a group."
   type        = list
 }
+
+variable "create_circleci_role" {
+  description = "Create IAM role for CircleCI user to assume."
+  type        = bool
+  default     = false
+}
