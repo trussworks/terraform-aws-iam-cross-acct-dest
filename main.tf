f9e28e033b2fdd7ad46d572dc6cc58ed20f9d0e5
kilbergr <kilbergr@users.noreply.github.com>
2019-12-20 11:43:13 -0800

    fix terradocs

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy_document" "role_assume_role_policy" {
     # can either trust account or specific roles in the account
     principals {
       type        = "AWS"
-      identifiers = length(var.source_account_role_names) > 0  ? "${formatlist(format("arn:aws:iam::%s:role/%%s", var.source_account_id), var.source_account_role_names)}" : ["${format("arn:aws:iam::%s:root", var.source_account_id)}"]
+      identifiers = length(var.source_account_role_names) > 0 ? "${formatlist(format("arn:aws:iam::%s:role/%%s", var.source_account_id), var.source_account_role_names)}" : ["${format("arn:aws:iam::%s:root", var.source_account_id)}"]
     }
 
     # only allow folks with MFA
