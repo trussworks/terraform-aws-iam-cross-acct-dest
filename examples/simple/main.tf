35e8e754ae9ce09eeb8d3b9c13366d7947987163
Elizabeth Eady <eeeady@truss.works>
2020-09-01 17:34:06 -0700

    i think im adding example

---
+++ examples/simple/main.tf
@@ -0,0 +1,10 @@
+module "test_group_role" {
+  source            = "../../"
+  iam_role_name     = "${var.test_name}"
+  source_account_id = var.source_account_id
+}
+
+resource "aws_iam_role_policy_attachment" "test_iam_policy" {
+  role       = module.test_group_role.iam_role_name
+  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+}
